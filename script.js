f839997b6257325599ff7c4b8dfb31e5072446fa
onifade <onifadejoshua15@gmial.com>
2022-06-22 12:03:44 +0100

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 "use strict";
-$(`#submit `).click(function() {
-    alert("Here");
-    // let name = $("#name").val();
-    // let email = $("#email").val();
-    // let username = $("#username").val();
-    // let password = $("#password").val();
-    // if (name == " " || email == " " || username == " " || password == " ") {
-    //     swal({
-    //         title: "Fields Empty!!!",
-    //         text: "Please check the missing fields!",
-    //         icon: "warning",
-    //         button: "Ok",
-    //     });
-    // } else {
-    //     swal({
-    //         title: "Successfully Submitted!",
-    //         icon: "successful",
-    //         button: "Yes!",
-    //     });
-    // }
-});
+let submit =
+    $("#submit").click(function() {
+        alert("Here");
+        // let name = $("#name").val();
+        // let email = $("#email").val();
+        // let username = $("#username").val();
+        // let password = $("#password").val();
+        // if (name == " " || email == " " || username == " " || password == " ") {
+        //     swal({
+        //         title: "Fields Empty!!!",
+        //         text: "Please check the missing fields!",
+        //         icon: "warning",
+        //         button: "Ok",
+        //     });
+        // } else {
+        //     swal({
+        //         title: "Successfully Submitted!",
+        //         icon: "successful",
+        //         button: "Yes!",
+        //     });
+        // }
+    });
